535ef86817773268a71a65133cc05630c9f0641f
Kimchi Rice <36904351+kimchirice@users.noreply.github.com>
2020-08-07 13:13:59 +1000

    Create Rim.js

---
+++ src/Components/Rim/Rim.js
@@ -0,0 +1,33 @@
+import React,{ useContext } from 'react'
+import { WheelContext } from '../../Context/WheelContext'
+
+const Rim = () =>{
+  const {wheel, handleChange} = useContext(WheelContext);
+  
+    return (            
+      <div>
+        <h3>Rim details</h3>
+        <form class="">
+          <fieldset id="form--rim">
+            <div className="form-row">
+              <div className="form-group col-md-7">
+                <label class="form-label" htmlFor="rimName">Rim Name</label>
+                <input 
+                    className="form-control"
+                    type='text'
+                    id='rimName' 
+                    name='rimName' 
+                    value={wheel.rimName} 
+                    onChange={handleChange} 
+                />
+                <p className=""> optional</p>
+              </div>
+            </div>
+          </fieldset>
+          
+        </form>
+      </div>
+    );
+}
+
+export default Rim
