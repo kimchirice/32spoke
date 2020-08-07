2698b4afbfbd4744c92a2423fee5e19635778113
Kimchi Rice <36904351+kimchirice@users.noreply.github.com>
2020-08-07 10:13:57 +1000

    Create WheelContext.js

---
+++ src/Context/WheelContext.js
@@ -0,0 +1,31 @@
+import React, { Children } from 'react'
+import { createContext } from 'react'
+
+const WheelContext = createContext()
+
+function WheelContextProvider({children}) {
+  const initialWheelValue = {
+    hubName: '',
+    rimName: '',
+    spokeName: ''
+  }
+
+  const [currentStep, setCurrentStep] = useState(1)
+  const [wheel, setWheel] = useState(initialWheelValue)
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setWheel({
+      ...wheel,
+      [name]: value
+    })
+  }
+
+  return (
+    <WheelContextProvider value={{currentStep, wheel, handleChange}}>
+      {children}
+    </WheelContextProvider>
+  )
+}
+
+export { WheelContext, WheelContextProvider }
