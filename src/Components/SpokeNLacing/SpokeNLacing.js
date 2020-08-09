5894e77e3253fdebb3506d5849e60c1554492b4e
Kimchi Rice <kimchirice@sina.com>
2020-08-10 00:24:40 +1000

    [Mod] syle in spoke & lacing form

--- src/Components/SpokeNLacing/SpokeNLacing.js
+++ src/Components/SpokeNLacing/SpokeNLacing.js
@@ -8,9 +8,10 @@ export default function SpokeNLacing() {
         <h3>Spoke & Lacing</h3>
         <form >
           <fieldset id="form--spokeNLacing">
-            <div className="form-row">
-              <div className="form-group">
-                <label className="form-label" htmlFor="spokeName">Spoke Name</label>
+           
+              <div className="form-group row">
+                <label className="col-form-label  col-sm-4" htmlFor="spokeName">Spoke Name</label>
+                <div className="col-md-8">
                 <input 
                     className="form-control"
                     type='text'
@@ -19,13 +20,15 @@ export default function SpokeNLacing() {
                     value={wheel.spokeName} 
                     onChange={handleChange} 
                 />
-                <p className=""> optional</p>
+                  </div>
               </div>
-            </div>
+        
+           
 
-            <div className="form-row">
-              <div className="form-group ">
-                <label className="form-label" htmlFor="spokeWeight">Spoke Weight</label>
+            
+            <div className="form-group row">
+              <label className="col-form-label  col-sm-4" htmlFor="spokeWeight">Spoke Weight(g.)</label>
+              <div className="col-sm-8">  
                 <input 
                     className="form-control"
                     type='text'
@@ -36,17 +39,18 @@ export default function SpokeNLacing() {
                 />
               </div>
             </div>
-            <div className="form-row">
-              <div className="form-group ">
-                <label className="form-label" htmlFor="numberOfSpokes">Number of Spokes</label>
+           
+            <div className="form-group row">
+              <label className="col-form-label col-sm-6 ml-0 pl-0 font-weight-bold" htmlFor="numberOfSpokes">Number of Spokes</label>
+              <div className="col-sm-6">
                 <select 
                   className="form-control"
                   type='text'
                   id='numberOfSpokes' 
                   name='numberOfSpokes' 
                   value={wheel.numberOfSpokes} 
-                  onChange={handleChange} >
-                  
+                  onChange={handleChange}
+                >
                   <option value="16">16</option>
                   <option value="18">18</option>
                   <option value="20">20</option>
@@ -54,50 +58,48 @@ export default function SpokeNLacing() {
                   <option value="28">28</option>
                   <option value="32" selected>32</option>
                 </select>
-                    
-              </div>
+              </div> 
             </div>
-            <div className="form-row">
-              <div className="form-group ">
-                <label className="form-label" htmlFor="numberOfSpokes">Number of Spokes</label>
-                <select 
-                  className="form-control"
-                  type='text'
-                  id='numberOfSpokes' 
-                  name='numberOfSpokes' 
-                  value={wheel.numberOfSpokes} 
-                  onChange={handleChange} >
-                  
-                  <option value="0">ridial</option>
-                  <option value="1">x1</option>
-                  <option value="2">x2</option>
-                  <option value="3" selected>x3</option>
-                  <option value="4">x4</option>
-                </select>
+        
+              <div className="form-group row">
+                <label className="col-form-label col-sm-6 ml-0 pl-0 font-weight-bold" htmlFor="lacingPatternLeft">Lacing Pattern Left</label>
+                <div className="col-sm-6">  
+                  <select 
+                    className="form-control"
+                    type='text'
+                    id='lacingPatternLeft' 
+                    name='lacingPatternLeft' 
+                    value={wheel.lacingPatternLeft} 
+                    onChange={handleChange} >
                     
+                    <option value="0">ridial</option>
+                    <option value="1">x1</option>
+                    <option value="2">x2</option>
+                    <option value="3" selected>x3</option>
+                    <option value="4">x4</option>
+                  </select>    
+                </div>
               </div>
-            </div>
-            <div className="form-row">
-              <div className="form-group ">
-                <label className="form-label" htmlFor="numberOfSpokes">Lacing pattern right</label>
-                <select 
-                  className="form-control"
-                  type='text'
-                  id='lacingPatternRight' 
-                  name='lacingPatternRight' 
-                  value={wheel.lacingPatternRight} 
-                  onChange={handleChange} >
-                  
-                  <option value="0">ridial</option>
-                  <option value="1">x1</option>
-                  <option value="2">x2</option>
-                  <option value="3" selected>x3</option>
-                  <option value="4">x4</option>
-                </select>
-                    
-              </div>
-            </div>
 
+              <div className="form-group row">
+                <label className="col-form-label col-sm-6 ml-0 pl-0 font-weight-bold" htmlFor="numberOfSpokes">Lacing pattern right</label>
+                <div className="col-sm-6">
+                  <select 
+                    className="form-control"
+                    type='text'
+                    id='lacingPatternRight' 
+                    name='lacingPatternRight' 
+                    value={wheel.lacingPatternRight} 
+                    onChange={handleChange} >
+                    
+                    <option value="0">ridial</option>
+                    <option value="1">x1</option>
+                    <option value="2">x2</option>
+                    <option value="3" selected>x3</option>
+                    <option value="4">x4</option>
+                  </select>
+                </div>   
+              </div>
           </fieldset>
         </form>
       </div>
