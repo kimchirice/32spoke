758ae024e3260ca6c26c39a807ba90376d6e2fa9
Kimchi Rice <kimchirice@sina.com>
2020-08-12 21:30:56 +1000

    [Add]

---
+++ src/Components/Input/Input1.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Input1(props) {
+  return (
+    <div className="form-row">
+      <div className="form-group">
+        <label className="form-label" htmlFor={props.name}>{props.title}</label>
+        <input 
+          className="form-control"
+          name={props.name}
+          id={props.name}
+          placeholder={props.placeholder}
+          value={props.value}
+          onChange={props.handleChange}
+          {...props}
+        />
+      </div>
+    </div>
+  )
+}
+
+
+
+export default Input1 
