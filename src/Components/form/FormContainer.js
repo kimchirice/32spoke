11df99ba2502f8049c4a3aa7e0ecfd56d46301e5
kimchirice <kimchi@u12.co>
2020-08-24 11:47:56 +1000

    [Mod] move Steps and Buttons to outside render(){} functions

--- src/Components/form/FormContainer.js
+++ src/Components/form/FormContainer.js
@@ -21,7 +21,7 @@ class FormContainer extends Component {
   }
 
   handleChange(event) {
-    const { name, value } = event.target;
+    const {name, value} = event.target;
     this.setState({
       wheel: {
         ...this.state.wheel, 
@@ -36,17 +36,23 @@ class FormContainer extends Component {
   
   handleClickNext(e) {
     if (this.state.currentStep >= 1 && this.state.currentStep < 3) {
-      this.setState(prevState => prevState.currentStep += 1)
+      this.setState( prevState => ({
+        currentStep: prevState.currentStep =+ 1,
+      })
+      )
     }
   }
   handleClickPrev(e) {
     e.preventDefault();
     if (this.state.currentStep >= 2) {
-      this.setState(prevState => prevState.currentStep -= 1)
-    }
+      this.setState( prevState => ({
+        currentStep: prevState.currentStep -= 1,
+      })
+      )
   }
 
   render() {
+
     const Steps = () => {
       switch(this.state.currentStep) {
         case 1: 
@@ -62,7 +68,7 @@ class FormContainer extends Component {
             <Hub/>
           );
       }
-    }
+    };
     
     
     const Buttons = () => {
@@ -93,7 +99,8 @@ class FormContainer extends Component {
             </div>
           )
       }
-    }
+    };
+
     return (
       <div className="d-flex flex-column justify-content-center align-items-start">
         <h4>Step {this.state.currentStep}</h4>
@@ -104,4 +111,4 @@ class FormContainer extends Component {
   }
 }
 
-export default FormContainer
+export default FormContainer;
--- src/Components/form/FormSpokeNLacing.js
+++ src/Components/form/FormSpokeNLacing.js
@@ -1,6 +1,5 @@
 import React from 'react'
 
-
 export default function SpokeNLacing(props) {
   return (            
       <div>
