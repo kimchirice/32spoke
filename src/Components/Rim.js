80db2b49215a4f2235cbbbcedd9820f7992abeaa
kimchirice <kimchi@u12.co>
2020-08-28 20:42:18 +1000

    [Mod] add related input fields to Rim.js, and remove the file FormRim.js

--- src/Components/Rim.js
+++ src/Components/Rim.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 const Rim = ({handleChange})=> {
   return (
+    <>
     <div>
       <label htmlFor="rimName" className="form-label">Rim Name</label>
       <input
@@ -12,6 +13,46 @@ const Rim = ({handleChange})=> {
         onChange={(e) => handleChange(e.target.value)}
         />
     </div>
+
+    <div className="form-group ">
+      <label className="form-label" htmlFor="rimWeight">Rim Weight</label>
+      <input 
+          className="form-control"
+          type='text'
+          id='rimWeight' 
+          name='rimWeight' 
+          onChange={(e) => handleChange(e.target.value)}          />
+    </div>
+   
+      <div className="form-group ">
+        <label className="form-label" htmlFor="rimERD">Rim E.R.D</label>
+        <input 
+            className="form-control"
+            type='text'
+            id='rimERD' 
+            name='rimERD' 
+            onChange={(e) => handleChange(e.target.value)}          />
+      </div>
+
+      <div className="form-group ">
+        <label className="form-label" htmlFor="offsetSpokeBed">Offset Spoke Bed</label>
+        <input 
+            className="form-control"
+            type='text'
+            id='offsetSpokeBed' 
+            name='offsetSpokeBed' 
+            onChange={(e) => handleChange(e.target.value)}          />
+      </div>
+      <div className="form-group ">
+        <label className="form-label" htmlFor="maxRimTension">Max Rim Tension</label>
+        <input 
+            className="form-control"
+            type='text'
+            id='maxRimTension' 
+            name='maxRimTension' 
+            onChange={(e) => handleChange(e.target.value)}          />
+      </div>
+    </>
   )
 }
 
