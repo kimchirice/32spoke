88ec645e7784212559b23e68d9f89fb5c6b475d6
Kimchi Rice <kimchirice@sina.com>
2020-09-09 15:23:52 +1000

    [Mod] adding input field unit to Rim.js, [Rev] field unit from label

--- src/Components/Rim.js
+++ src/Components/Rim.js
@@ -18,45 +18,65 @@ const Rim = ({handleChange, rim})=> {
 
     <div className="form-group ">
       <label className="form-label" htmlFor="rimWeight">Rim Weight</label>
-      <input 
+      <div className="d-flex">  
+        <input 
           className="form-control"
           type='text'
           id='rimWeight' 
           name='rimWeight'
           value={rim.rimWeight} 
-          onChange={handleChange}          />
+          onChange={handleChange}/>
+          <div className="form-group-append">
+            <span className="input-group-text">g</span>
+          </div>
+        </div>
     </div>
    
       <div className="form-group ">
         <label className="form-label" htmlFor="rimERD">Rim E.R.D</label>
-        <input 
+        <div className="d-flex">
+          <input 
             className="form-control"
             type='text'
             id='rimERD' 
             name='rimERD'
             value={rim.rimERD} 
-            onChange={handleChange}          />
+            onChange={handleChange} />
+          <div className="form-group-append">
+            <span className="input-group-text">mm</span>
+          </div>
+        </div>
       </div>
 
       <div className="form-group ">
-        <label className="form-label" htmlFor="offsetSpokeBed">Offset Spoke Bed</label>
-        <input 
+        <label className="form-label" htmlFor="offsetSpokeBed">Offset Spoke Bed(OSB)</label>
+        <div className="d-flex">  
+          <input 
             className="form-control"
             type='text'
             id='offsetSpokeBed' 
             name='offsetSpokeBed'
             value={rim.offsetSpokeBed} 
             onChange={handleChange}          />
+          <div className="form-group-append">
+            <span className="input-group-text">mm</span>
+          </div>
+        </div>
       </div>
       <div className="form-group ">
         <label className="form-label" htmlFor="maxRimTension">Max Rim Tension</label>
-        <input 
+        <div className="d-flex">  
+          <input 
             className="form-control"
             type='text'
             id='maxRimTension' 
             name='maxRimTension'
             value={rim.maxRimTension} 
             onChange={handleChange}          />
+          <div className="form-group-append">
+            <span className="input-group-text">kgF</span>
+          </div>
+        </div>
       </div>
     </>
   )
