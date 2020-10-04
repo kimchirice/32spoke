e71e67e67ca7c395af7d941ef48937f21dc4c22c
kimchirice <kimchi@u12.co>
2020-10-05 10:51:27 +1100

    [Mod]

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Navigation from './components/Navigation';
+import Navigation from './Components/Navigation';
 
 function App() {
     return (
