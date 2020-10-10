1650be6e74aa812d151c39af8d8dc323bf62100a
Kimchi Rice <36904351+kimchirice@users.noreply.github.com>
2020-10-10 13:12:39 +1100

    Revert "Debug spoke length calculation"

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -118,32 +118,16 @@ function Home() {
     if (currentStep < 4){
       setCurrentStep( preV => preV += 1)
     }
-    // calculate left side, if it is a rear hub,  this is of non-drive side
+    // calculate left side
     // d, r1, r2, r3, m, k
     let centerToFlangeLeft = parseFloat(hub.leftFlangeToCenter)
-    console.log(`centerToFlangeLeft is ${centerToFlangeLeft}`)
-    // 39.22 * 39.22 = 1538.2084
     let centerToFlangeRight = parseFloat(hub.rightFlangeToCenter)
-    console.log(`centerToFlangeRight is ${centerToFlangeRight}`)
-
     let m = parseFloat(spoke.numberOfSpokes) / 2
-    console.log(`m is ${m}`)
-    // 16 if it is of a 32-spoke wheel
     let kLeft = parseFloat(spoke.lacingPatternLeft)
-    // 0 if lacing pattern is radial
-    console.log(`kLeft is ${kLeft}` )
     let kRight = parseFloat(spoke.lacingPatternRight)
-    // 3 if lacing pattern is 3-cross
-    console.log(`kRight is ${kRight}`)
     let radius1 = parseFloat(hub.leftFlangeDiameter) / 2
-    // 16 mm. 16 x 16 = 256
-    console.log(`r1 is ${radius1}`)
     let radius2 = parseFloat(rim.rimERD) / 2
-    // 294 * 294 =86436
-    console.log(`r2 is ${radius2}`)
     let radius3 = parseFloat(hub.spokeHoleDiameter) / 2
-    // 2.6 / 2 = 1.3
-    console.log(`r3 is ${radius3}`)
 
     const spokeLengthLeftUpdate = SpokeCalculator(
       centerToFlangeLeft, 
