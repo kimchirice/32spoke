aae315ca83ea0174cbcf7dd3ae75a4ec64da5a97
kimchirice <kimchi@u12.co>
2020-10-14 13:32:24 +1100

    [Mod] update details of input fields

--- src/Components/Rim/Rim.js
+++ src/Components/Rim/Rim.js
@@ -1,87 +1,85 @@
-import React,{ useContext } from 'react'
-import { WheelContext } from '../../Context/WheelContext'
+import React from 'react'
 
-const Rim = () =>{
-  const {wheel, handleChange} = useContext(WheelContext);
-  
-    return (            
+const Rim = ({handleChange, rim})=> {
+  return (
+    <>
       <div>
-        <h3>Rim details</h3>
-        <form >
-          <fieldset id="form--rim">
-            <div className="form-row">
-              <div className="form-group">
-                <label className="form-label" htmlFor="rimName">Rim Name</label>
-                <input 
-                    className="form-control"
-                    type='text'
-                    id='rimName' 
-                    name='rimName' 
-                    value={wheel.rimName} 
-                    onChange={handleChange}
-                    autoFocus 
-                />
-                <p className=""> optional</p>
-              </div>
-            </div>
-            <div className="form-row">
-              <div className="form-group ">
-                <label className="form-label" htmlFor="rimWeight">Rim Weight</label>
-                <input 
-                    className="form-control"
-                    type='text'
-                    id='rimWeight' 
-                    name='rimWeight' 
-                    value={wheel.rimWeight} 
-                    onChange={handleChange} 
-                />
-              </div>
-            </div>
-            <div className="form-row">
-              <div className="form-group ">
-                <label className="form-label" htmlFor="rimERD">Rim E.R.D</label>
-                <input 
-                    className="form-control"
-                    type='text'
-                    id='rimERD' 
-                    name='rimERD' 
-                    value={wheel.rimERD} 
-                    onChange={handleChange} 
-                />
-              </div>
-            </div>
-            <div className="form-row">
-              <div className="form-group ">
-                <label className="form-label" htmlFor="offsetSpokeBed">Offset Spoke Bed</label>
-                <input 
-                    className="form-control"
-                    type='text'
-                    id='offsetSpokeBed' 
-                    name='offsetSpokeBed' 
-                    value={wheel.offsetSpokeBed} 
-                    onChange={handleChange} 
-                />
-              </div>
-            </div>
-            <div className="form-row">
-              <div className="form-group ">
-                <label className="form-label" htmlFor="maxRimTension">Max Rim Tension</label>
-                <input 
-                    className="form-control"
-                    type='text'
-                    id='maxRimTension' 
-                    name='maxRimTension' 
-                    value={wheel.maxRimTension} 
-                    onChange={handleChange} 
-                />
-              </div>
-            </div>
-
-          </fieldset>
-          
-        </form>
+        <label htmlFor="rimName" className="form-label">Rim Name</label>
+        <input
+          className="form-control"
+          type='text'
+          id="rimName"
+          name='rimName'
+          value={rim.rimName}
+          onChange={handleChange}
+          autoFocus
+          />
       </div>
-    );
+
+      <div className="form-group ">
+        <label className="form-label" htmlFor="rimWeight">Rim Weight</label>
+        <div className="d-flex">  
+          <input 
+            className="form-control"
+            type='text'
+            id='rimWeight' 
+            name='rimWeight'
+            value={rim.rimWeight} 
+            onChange={handleChange}/>
+            <div className="form-group-append">
+              <span className="input-group-text">g</span>
+            </div>
+          </div>
+      </div>
+   
+      <div className="form-group ">
+        <label className="form-label" htmlFor="rimERD">Rim E.R.D</label>
+        <div className="d-flex">
+          <input 
+            className="form-control"
+            type='text'
+            id='rimERD' 
+            name='rimERD'
+            value={rim.rimERD} 
+            onChange={handleChange} />
+          <div className="form-group-append">
+            <span className="input-group-text">mm</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="form-group ">
+        <label className="form-label" htmlFor="offsetSpokeBed">Offset Spoke Bed(OSB)</label>
+        <div className="d-flex">  
+          <input 
+            className="form-control"
+            type='text'
+            id='offsetSpokeBed' 
+            name='offsetSpokeBed'
+            value={rim.offsetSpokeBed} 
+            onChange={handleChange}          />
+          <div className="form-group-append">
+            <span className="input-group-text">mm</span>
+          </div>
+        </div>
+      </div>
+      <div className="form-group ">
+        <label className="form-label" htmlFor="maxRimTension">Max Rim Tension</label>
+        <div className="d-flex">  
+          <input 
+            className="form-control"
+            type='text'
+            id='maxRimTension' 
+            name='maxRimTension'
+            value={rim.maxRimTension} 
+            onChange={handleChange}          />
+          <div className="form-group-append">
+            <span className="input-group-text">kgF</span>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default Rim
+export default Rim;
