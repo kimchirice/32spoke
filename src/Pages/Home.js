e61b12448d8b6453dd31997a0d2a0d0fbc04123d
kimchirice <kimchi@u12.co>
2020-10-14 16:10:29 +1100

    [Mod] update paths in Home, then fix bug of calculation fomula

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
-import Progress from '../components/Progress';
-import Hub from '../components/Hub';
-import Rim from '../components/Rim';
-import Spoke from '../components/Spoke';
-import Button from '../components/Button';
-import SpokeCalculator from '../components/utils/SpokeCalculator';
-import SpokeLength from '../components/SpokeLength';
+import Progress from '../components/progress/Progress';
+import Hub from '../components/hub/Hub';
+import Rim from '../components/rim/Rim';
+import Spoke from '../components/spoke/Spoke';
+import Button from '../components/button/Button';
+import SpokeCalculator from '../utils/SpokeCalculator';
+import SpokeLength from '../components/spokeLength/SpokeLength';
 
 function Home() {
   let spokeLengthRight
--- src/utils/SpokeCalculator.js
+++ src/utils/SpokeCalculator.js
@@ -22,7 +22,7 @@ const spokeCalculator=(d, r1, r2, r3, m, k) => {
 
   const angle = angleCalculator(k, m)
 
-  let spokeLength = Math.sqrt(d*d + r1*r1 + r2*r2 - 2*r1*r2*Math.cos(angle)) - (r3/)
+  let spokeLength = Math.sqrt(d*d + r1*r1 + r2*r2 - 2*r1*r2*Math.cos(angle)) - r3
   
   spokeLength = roundNumberToTowDecimal(spokeLength)
   
