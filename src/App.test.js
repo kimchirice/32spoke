41ec83be1fa691e9cdfc48acb7d79465d9c44329
Kimchi Rice <kimchirice@sina.com>
2020-12-08 14:48:18 +1100

    [Add] 2 test cases

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders hub Name input', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const inputElement = getByText(/Hub name/i);
+  expect(inputElement).toBeInTheDocument();
+});
+
+test('renders hub weight input', () => {
+  const { getByText } = render(<App />);
+  const inputElement = getByText(/Hub weight/i);
+  expect(inputElement).toBeInTheDocument();
 });
