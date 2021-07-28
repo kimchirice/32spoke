e7ea5b76a9554a993cbce8efa87e1af8b257590f
Max Du <kimchi@u12.co>
2021-07-28 14:06:44 +1000

    [Add] install eslint [Add] install prettier [Add] install eslint-config-prettier [Add] a new file .prettierrc.json [Add] a new file .prettierignore [Mod] .eslint.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,8 @@ module.exports = {
   },
   "extends": [
     "eslint:recommended",
-    "plugin:react/recommended"
+    "plugin:react/recommended",
+    "prettier"
   ],
   "parserOptions": {
     "ecmaFeatures": {
@@ -20,30 +21,11 @@ module.exports = {
     "react", "jest"
   ],
   "rules": {
-    "indent": [
-      "error",
-      2
-    ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
     "quotes": [
       "error",
       "single"
     ],
-    "semi": [
-      "error",
-      "never"
-    ],
     "eqeqeq": "error",
-    "no-trailing-spaces": "error",
-    "object-curly-spacing": [
-      "error", "always"
-    ],
-    "arrow-spacing": [
-      "error", { "before": true, "after": true }
-    ],
     "no-console": 0,
     "react/prop-types": 0
   },
